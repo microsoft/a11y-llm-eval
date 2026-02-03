f7d6ae0320cf7ba10fbfa694b60fb7d470fd6d9a
Michael Fairchild <mfairchild365@proton.me>
2026-02-03 15:56:21 -0600

    Fix "Each text input has textbox role" edge case
    This was sometimes failing because the selector was incorrecting identifying some form wrappers (such as .field-error) even when they did not contain a form control.

--- node_runner/helpers/get-form-field-wrapper.js
+++ node_runner/helpers/get-form-field-wrapper.js
@@ -1,8 +1,17 @@
 const FIELD_WRAPPER_SELECTOR = '[class*="field"], [class*="input"], [class*="control"], [class*="item"], [class*="group"]';
 
+// Only treat elements as "field wrappers" if they actually wrap an interactive control.
+// This avoids counting helper/error text nodes like `<span class="field-error">...` as wrappers
+// simply because their class name contains "field".
+//
+// Include `contenteditable` because some examples intentionally implement "text inputs" that way;
+// those should still be considered fields for role-related assertions.
+const CONTROL_SELECTOR = 'input, textarea, select, button, [role="textbox"], [role="button"], [contenteditable]:not([contenteditable="false"])';
+
 // Get all form field wrapper elements within the given scope as Playwright locators
 const getAllFormFieldWrappers = async (scope) => {
-    return await scope.locator(FIELD_WRAPPER_SELECTOR);
+    const wrappers = scope.locator(FIELD_WRAPPER_SELECTOR);
+    return wrappers.filter({ has: scope.locator(CONTROL_SELECTOR) });
 };
 
 module.exports.getAllFormFieldWrappers = getAllFormFieldWrappers;
