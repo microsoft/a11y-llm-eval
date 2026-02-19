8d451a645adaf22b595e54a4f313d64d45b37643
Michael Fairchild <mfairchild365@proton.me>
2026-02-19 08:15:45 -0600

    Fix form field selector

--- node_runner/helpers/get-form-field-wrapper.js
+++ node_runner/helpers/get-form-field-wrapper.js
@@ -1,12 +1,6 @@
-const FIELD_WRAPPER_SELECTOR = '[class*="field"], [class*="input"], [class*="control"], [class*="item"], [class*="group"]';
-
-// Only treat elements as "field wrappers" if they actually wrap an interactive control.
-// This avoids counting helper/error text nodes like `<span class="field-error">...` as wrappers
-// simply because their class name contains "field".
-//
-// Include `contenteditable` because some examples intentionally implement "text inputs" that way;
-// those should still be considered fields for role-related assertions.
-const CONTROL_SELECTOR = 'input, textarea, select, button, [role="textbox"], [role="button"], [contenteditable]:not([contenteditable="false"]), *:has(> :is(input:not([type=button],[type=submit],[type=reset]), textarea, select))';
+const CONTROL_SELECTOR = 'input, textarea, select, button, [role="textbox"], [role="button"], [contenteditable]:not([contenteditable="false"])';
+const FIELD_SELECTOR = CONTROL_SELECTOR.replace('button, ','').replace('input','input:not([type="submit"], [type="reset"], [type="button"])');
+const FIELD_WRAPPER_SELECTOR = `[class*="field"], [class*="input"], [class*="control"], [class*="item"], [class*="group"], *:has(> :is(${FIELD_SELECTOR}))`;
 
 // Get all form field wrapper elements within the given scope as Playwright locators
 const getAllFormFieldWrappers = async (scope) => {
